e8327ef5c8bdcd9c3a6b16234e760a29dca66621
jonas <pottiejonas@gmail.com>
2013-03-28 23:05:50 +0100

    arrowADDED

--- smarty_compile/bf52392aafed985f31aabc7eb1f5c408605ab465.file.index.htm.php
+++ smarty_compile/bf52392aafed985f31aabc7eb1f5c408605ab465.file.index.htm.php
@@ -1,61 +1,29 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<?php /* Smarty version Smarty-3.1.8, created on 2013-03-28 06:09:48
-=======
-<?php /* Smarty version Smarty-3.1.8, created on 2013-03-28 16:04:48
->>>>>>> 69ac9a1366de0711b84025f09e99ed01bc65d8a2
-=======
-<?php /* Smarty version Smarty-3.1.8, created on 2013-03-28 17:00:09
->>>>>>> 4c5a26c63abf8feb379c2917504956bbc65a5869
-=======
-<?php /* Smarty version Smarty-3.1.8, created on 2013-03-28 17:28:31
->>>>>>> kak
-=======
-<?php /* Smarty version Smarty-3.1.8, created on 2013-03-28 17:16:28
->>>>>>> buildings-touched images
+<?php /* Smarty version Smarty-3.1.8, created on 2013-03-28 22:59:08
          compiled from "smarty_template/index.htm" */ ?>
-<?php /*%%SmartyHeaderCode:163906304751546cdc4f5b52-79969866%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:189391344351547191a96363-51059666%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     'bf52392aafed985f31aabc7eb1f5c408605ab465' => 
     array (
       0 => 'smarty_template/index.htm',
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-      1 => 1364394629,
-=======
-      1 => 1364483087,
->>>>>>> 69ac9a1366de0711b84025f09e99ed01bc65d8a2
-=======
-      1 => 1364486336,
->>>>>>> 4c5a26c63abf8feb379c2917504956bbc65a5869
-=======
-      1 => 1364488109,
->>>>>>> kak
-=======
-      1 => 1364486874,
->>>>>>> buildings-touched images
+      1 => 1364507947,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '163906304751546cdc4f5b52-79969866',
+  'nocache_hash' => '189391344351547191a96363-51059666',
   'function' => 
   array (
   ),
+  'version' => 'Smarty-3.1.8',
+  'unifunc' => 'content_51547191b3d367_80311426',
   'variables' => 
   array (
     'content' => 0,
   ),
   'has_nocache_code' => false,
-  'version' => 'Smarty-3.1.8',
-  'unifunc' => 'content_51546cdc5040a8_93951368',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_51546cdc5040a8_93951368')) {function content_51546cdc5040a8_93951368($_smarty_tpl) {?><!DOCTYPE html>
+<?php if ($_valid && !is_callable('content_51547191b3d367_80311426')) {function content_51547191b3d367_80311426($_smarty_tpl) {?><!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
@@ -86,7 +54,7 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 <div id="container">
     <div id="startPage">
         <div class="menuBar">
-            <div class="info"><p>id</p></div>
+            <div class="info"><p>i</p></div>
             <div class="title"><p>Dag uitstippelen</p></div>
             <div class="dagtripIcon"><span>0</span></div>
         </div>
@@ -95,29 +63,11 @@ $_valid = $_smarty_tpl->decodeProperties(array (
     <div id="infoContent" class="infoBar">
     </div>
 
+            <div class="infoBarIII">FOTO</div>
+            <div class="infoBarIV"><h2>De vuurtoren</h2><p>Straatnaam 432 Oostende</p></div>
+            <div class="infoBarArrow"><img src="assets/images/arrow.png" width="17"></div>
+            <div class="infoBarV"><div class="Minibutten"> + toevoegen aan dagtrip</div></div>
 
-    <div class="dagtripItem">
-            <div class="dagtripIcontje">F2</div>
-            <div class="dagtripTitel"><h2>De vuurtoren</h2><p>Straatnaam 432 Oostende</p></div>
-            <div class="dagtripColor"></div>
-        </div>
-
-        <div class="dagtripItem">
-            <div class="dagtripIcontje">F2</div>
-            <div class="dagtripTitel"><h2>Restaurant</h2><p>Straatnaam 432 Oostende</p></div>
-            <div class="dagtripColor"></div>
-        </div>
-
-        <div class="dagtripItem">
-            <div class="dagtripIcontje">F2</div>
-            <div class="dagtripTitel"><h2>Wellnesscentrum</h2><p>Straatnaam 432 Oostende</p></div>
-            <div class="dagtripColor"></div>
-        </div>
-
-        
-    
-
-    <div id="content" class="buildings">
 
         <?php echo $_smarty_tpl->tpl_vars['content']->value;?>
 
--- smarty_compile/fe1860931442cbf44fa460920924767f31b40218.file.home.tpl.php
+++ smarty_compile/fe1860931442cbf44fa460920924767f31b40218.file.home.tpl.php
@@ -1,25 +1,25 @@
-<?php /* Smarty version Smarty-3.1.8, created on 2013-03-28 17:16:28
+<?php /* Smarty version Smarty-3.1.8, created on 2013-03-28 17:36:33
          compiled from "smarty_template/pages/home.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:113703667151546cdc4bcfc5-27792278%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:61130120251547191a79f55-39240363%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     'fe1860931442cbf44fa460920924767f31b40218' => 
     array (
       0 => 'smarty_template/pages/home.tpl',
-      1 => 1364383567,
+      1 => 1364383646,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '113703667151546cdc4bcfc5-27792278',
+  'nocache_hash' => '61130120251547191a79f55-39240363',
   'function' => 
   array (
   ),
   'has_nocache_code' => false,
   'version' => 'Smarty-3.1.8',
-  'unifunc' => 'content_51546cdc4eed20_82636065',
+  'unifunc' => 'content_51547191a91031_06133180',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_51546cdc4eed20_82636065')) {function content_51546cdc4eed20_82636065($_smarty_tpl) {?>
+<?php if ($_valid && !is_callable('content_51547191a91031_06133180')) {function content_51547191a91031_06133180($_smarty_tpl) {?>
 <div id="subContent">
 
 </div><?php }} ?>
